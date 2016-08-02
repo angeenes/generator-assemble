06f221c9bc77092d4999a20e9e964513dfe6a523
angeenes <nelson.patrao@gmail.com>
2016-08-02 19:08:23 +0200

    Updated watch task

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
     watch: {
       assemble: {
         files: ['<%%= config.src %>/{content,data,templates}/{,*/}*.{md,hbs,yml}'],
-        tasks: ['assemble']
+        tasks: ['clean', 'stylus', 'uglify', 'assemble']
       },
       livereload: {
         options: {
